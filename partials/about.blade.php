6d7b177e06ed9c99edb91595cfc46f520f87f1ad
Suresh Marru <smarru@apache.org>
2017-03-27 13:17:02 -0400

    Adding application input files

--- partials/about.blade.php
+++ partials/about.blade.php
@@ -3,5 +3,10 @@
     <div class="text-center">
       <h2 class="title top-space">About page</h2>            
     </div>
+
+      <p>
+          <a target="_blank" href="https://cwiki.apache.org/confluence/display/AIRAVATA/Tutorial+Application+Input+Files+and+Parameters">Sample Application Input Files</a>
+      </p>
+
   </div>  
 </div>
